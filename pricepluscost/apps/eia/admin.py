53640f2c8088d1491679b230cb522016de6f1a84
Phi <13445760+pdashk@users.noreply.github.com>
2020-04-28 22:46:14 -0700

    redo eia model to calculated rolling average instead of monthly values

--- pricepluscost/apps/eia/admin.py
+++ pricepluscost/apps/eia/admin.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
-from .models import ElectricityRate
+from django.conf import settings
+from .models import State
 
-class ElectricityRateAdmin(admin.ModelAdmin):
-    list_display = (
+from eia.utils.update import update_state_data
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
         'series_id',
-        'state',
-        'year',
-        'month',
-        'value',
-        'active',
-        'update_date'
-    )
+        'iso3166',
+        'rolling_average',
+        'average_start_date',
+        'average_end_date',
+        'source_update_date',
+        'last_updated',
+    ]
 
-admin.site.register(ElectricityRate, ElectricityRateAdmin)
+    readonly_fields = ['last_updated']
+
+    actions = ['refresh_state_data', admin.actions.delete_selected]
+
+    def refresh_state_data(self, request, queryset):
+        
+        updates = []
+
+        for obj in queryset:
+            update = update_state_data(settings.EIA_API_KEY, obj)
+            updates.append(update)
+        
+        self.message_user(request, f"Successfully refreshed {sum(updates)}/{len(updates)} selected states")
+
+admin.site.register(State, StateAdmin)
+admin.site.disable_action('delete_selected')
--- pricepluscost/apps/eia/models.py
+++ pricepluscost/apps/eia/models.py
@@ -1,23 +1,14 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-class ElectricityRate(models.Model):
+class State(models.Model):
+    name = models.CharField(max_length=50, unique=True)
     series_id = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    month_year = models.DateField(verbose_name="Month-Year")
-    value = models.FloatField()
-    update_date = models.DateTimeField(verbose_name="Last Updated")
-    active = models.BooleanField()
+    iso3166 = models.CharField(max_length=20)
+    rolling_average = models.FloatField(verbose_name="Rolling Annual Average (cents per kWh for last 12 months)")
+    average_start_date = models.DateField()
+    average_end_date = models.DateField()
+    source_update_date = models.DateTimeField()
+    last_updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return self.series_id
-
-    def year(self):
-        return self.month_year.year
-    
-    def month(self):
-        return self.month_year.month
-
-    class Meta:
-        verbose_name = 'Electricity Rate'
-        verbose_name_plural = 'Electricity Rates'
+        return self.name
